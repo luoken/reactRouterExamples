7dadcef0bbf60d110be5efd017a5f8a3dd190ebc
Ken Luo <luozhirong@gmail.com>
2017-06-03 16:23:47 -0700

    revision

--- src/examples/Basic.js
+++ src/examples/Basic.js
@@ -9,12 +9,19 @@ const Home = () => (
   </div>
 )
 
-const About = () => (
+/*const About = () => (
   <div>
     <h2>About</h2>
   </div>
-)
+)*/
 
+function About() {
+  return(
+    <div>
+      <h2>About here</h2>
+    </div>
+  )
+}
 const Topic = ({ match }) => (
   <div>
     <h3>{match.params.topicId}</h3>
@@ -42,10 +49,10 @@ const Topics = ({ match }) => (
       </li>
     </ul>
 
-    <Route path={`${match.url}/:topicId`} component={Topic}/>
+    <Route path={`${match.url}/:topicId`} component={Topic} />
     <Route exact path={match.url} render={() => (
       <h3>Please select a topic.</h3>
-    )}/>
+    )} />
   </div>
 )
 
@@ -58,13 +65,13 @@ const Basic = () => (
         <li><Link to="/topics">Topics</Link></li>
       </ul>
 
-      <hr/>
+      <hr />
 
-      <Route exact path="/" component={Home}/>
-      <Route path="/about" component={About}/>
-      <Route path="/topics" component={Topics}/>
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/topics" component={Topics} />
     </div>
   </Router>
 )
 
-module.exports=Basic;
+module.exports = Basic;
